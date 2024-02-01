2f40d2509f8dca5d77d8c38460367ecdd85f555c
shriyabi <141756403+shriyabi@users.noreply.github.com>
2024-01-31 23:27:04 -0500

    js is p cool maybe i should have used php

--- Home.js
+++ Home.js
@@ -147,7 +147,22 @@ function showSlides(n) {
        }
        )
    })
-
+   window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Success!");
+      })
+    });
+  });
+  
 
 
 
